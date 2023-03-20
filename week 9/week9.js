3db1892cd868c7bc81132cc33e84020902e47c65
brianmwirigi <bmmaingi17@gmail.com>
2023-03-20 11:48:01 +0300

    javascript

--- week 9/week9.js
+++ week 9/week9.js
@@ -1,5 +1,15 @@
 var firstname = document.getElementById('first_name');
+var firstnameError = document.getElementById('firstname-error');
 
 firstname.addEventListener('input', function (e) {
-    console.console.log(e.target.value);
+    // console.log(e.target.value);
+    var pattern = /^[\W]{4,8}$/;
+    var currentValue = e.target.value;
+    var valid = pattern.test(currentValue);
+
+    if (valid) {
+        firstnameError.style.display = 'none'
+    } else {
+        firstnameError.style.display = 'block'
+    }
 })
