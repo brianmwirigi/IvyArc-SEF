c1bba8cd7c2a6751e9425a2d4512a215a6866585
brianmwirigi <bmmaingi17@gmail.com>
2023-03-29 10:33:13 +0300

    javascript scope

--- week 9/demo.js
+++ week 9/demo.js
@@ -28,3 +28,30 @@ console.log(x);
     console.log(x);
     //variable declared ni block is inaccessible outside the block
 }
+
+//hoisting
+/**
+ * the art of raising or lifting somethin
+ * ability to use variables before being defined
+ * only workd with the var keyword
+ * outputs error if let and const are used when calling varing before declared
+ * only workd for declaration and not initialization
+ * 
+ * 
+ * 
+ */
+
+x = 15; //initialization 
+console.log(x); //access of variable through the var keyword
+
+y = 17;
+//console.log(y); //tries to access the variable before declaration
+
+let y; //does not work with hoisting after declaration
+var x; //declaration as "var" 
+
+//constant must be initialized upon declaration (and cannot be reinitialized)
+
+const z = 19;
+
+document.getElementById("vaild").innerHTML += "<br>this call variable x" + "<br> whose output is" + x; //string concatination
