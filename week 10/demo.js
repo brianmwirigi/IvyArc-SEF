4b41048eb60b137c86d3b23daefb0e0eb77ef6d6
brianmwirigi <bmmaingi17@gmail.com>
2023-04-03 09:06:14 +0300

    javascript

---
+++ week 10/demo.js
@@ -0,0 +1,31 @@
+//object oriented programming
+// javascrift is a scripting and object oriented programming language
+//has classes vs objects
+/**
+ * object oriented programming enables easy definition and reuse of code
+ */
+/**
+ * each OOP blueprint is called a class and each instance of it is called an object
+ * each class presents a data structure blueprint that tells the computer-specific data to store and specific instructions on how to use the data
+ * data is stored in on computer memory called attributes/properties and instructions are stored as functions/methods
+ * each instance of a class ie object will inherit the variables(properties) and functions that are defined in its class and can use them freely
+ */
+/**
+ *  key OOP concepts
+ * Abstraction - meaning only revealing what is necessary to be known. each object only has access to the necessary data that exists within another object without the need to fully understand all the features
+ * Encapsulation - neatly packed data and functionality (methods) into a single entity which is a class. compliments abstraction and in data security
+ * Inheritance - OOP class being able to inherit the data and functionality of others. allows easy reuse of defined data and prevent unnecessary bloated programming code. inherited class is called a parent class (super-class) and the inheriting class is known as the child class(sub-class)
+ * Polymorphism - ability to inherit sub-classes to define and apply its properties nd functionality(method) while still inheriting and being able to freely utilise the functionality of its present class. can be used as an extension/by-product of inheritance
+ */
+
+/**
+ * constructions
+ * class is a blueprint while object is instance of a class
+ * a constructor is a specific javascript function found in a javascript class
+ * enables editing of javascript class create and store its methods
+ * it is required in creating objects from classes
+ * jin javascript, constructors are not explicitly named (even though it is a function)
+ * cunstructor - (keyword) is used
+ * later called using the new - (keyword) and class name 
+ * 
+ */
