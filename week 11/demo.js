578a3584be8aebaf8ff2add00d94ed0d03052e07
brianmwirigi <bmmaingi17@gmail.com>
2023-04-06 06:38:47 +0300

    javascript asynchronous

---
+++ week 11/demo.js
@@ -0,0 +1,15 @@
+//javascript asynchronous programming (callback, promises, await and async)
+/**
+ * to implement callback functions
+ * difference between synchronous and asynchronous
+ * purpose of asynchronous programming
+ * javascript promises and means of implementing
+ * understanding javascript await keyword and how it affects promises
+ * 
+ *
+ */
+
+/**
+ * callback function is a function passed as an argument to another function similar to how we use variables as arguments in function definitions
+ * it speeds up the process of coding by requiring a function to be provided as an argument that is used to perform activities using some output of the calling functions processin
+ */
